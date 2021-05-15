d29b260878a6e36c8b26c4a3c6206fb038931a0e
Nifu <frzmit@gmail.com>
2021-05-15 20:12:57 +1000

    footer design

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,12 +4,12 @@ import { StaticImage } from "gatsby-plugin-image";
 export default function Footer() {
   return (
     <footer className="bg-gray-100 mt-auto bottom-0">
-      <div className="global-wrapper py-0">
-        <div className="flex flex-wrap mb-4 space-x-10">
-          <StaticImage src="../images/footer/ACM Logo.png" alt="ACM Logo" height={50} />
-          <StaticImage src="../images/footer/SIGMM Logo.png" alt="SIGMM Logo" height={50} />
-          <StaticImage src="../images/footer/UQ Logo.png" alt="UQ Logo" height={50} />
-          <StaticImage src="../images/footer/Griffith Logo.png" alt="Griffith Logo" height={50} />
+      <div className="global-wrapper py-0 flex sm:block flex-col items-center md:items-start">
+        <div className="mb-4 flex flex-wrap justify-around md:justify-start md:space-x-10">
+          <StaticImage className="mb-4 md:mb-0" src="../images/footer/ACM Logo.png" alt="ACM Logo" height={50} />
+          <StaticImage className="mb-4 md:mb-0" src="../images/footer/SIGMM Logo.png" alt="SIGMM Logo" height={50} />
+          <StaticImage className="mb-4 md:mb-0" src="../images/footer/UQ Logo.png" alt="UQ Logo" height={50} />
+          <StaticImage className="mb-4 md:mb-0" src="../images/footer/Griffith Logo.png" alt="Griffith Logo" height={50} />
         </div>
         <small>© 1993-{new Date().getFullYear()} ACM Multimedia. All Rights Reserved.</small>
       </div>
