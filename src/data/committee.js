86aaf57420dc74c0eab1429cce56bca50c76469b
Nifu <frzmit@gmail.com>
2021-05-15 14:46:36 +1000

    Complete committee list

--- src/data/committee.js
+++ src/data/committee.js
@@ -10,7 +10,7 @@ const committee = [
       },
       {
         name: "Heng Tao Shen",
-        institution: "University of Electronic Science and Technology of China",
+        institution: "University of Electronic Science and Technology of China, China",
         imageUrl: "hengtao",
       },
     ],
@@ -20,13 +20,13 @@ const committee = [
     list: [
       {
         name: "Chang-wen Chen",
-        institution: "State University of New York at Buffalo, US",
-        imageUrl: "chang wen 2",
+        institution: "State University of New York at Buffalo, USA",
+        imageUrl: "chang wen_cropped",
       },
       {
         name: "Helen Huang",
         institution: "The University of Queensland, Australia",
-        imageUrl: "helen 2",
+        imageUrl: "helen_cropped",
       },
       {
         name: "Jun Zhou",
@@ -41,12 +41,12 @@ const committee = [
       {
         name: "Tatsuya Harada",
         institution: "The University of Tokyo, Japan",
-        imageUrl: "",
+        imageUrl: "tatsuya_cropped",
       },
       {
         name: "Jianfei Cai",
-        institution: "Monash University Australia",
-        imageUrl: "",
+        institution: "Monash University, Australia",
+        imageUrl: "jianfei_cropped",
       },
       {
         name: "Wu Liu",
@@ -61,22 +61,22 @@ const committee = [
     ],
   },
   {
-    role: "Local Chair",
+    role: "Local Chairs",
     list: [
       {
         name: "Sen Wang",
         institution: "The University of Queensland, Australia",
-        imageUrl: "",
+        imageUrl: "Sen Wang_cropped",
       },
       {
         name: "Tony Chen",
         institution: "The University of Queensland, Australia",
-        imageUrl: "",
+        imageUrl: "Tony Chen",
       },
     ],
   },
   {
-    role: "Web Chair",
+    role: "Web Chairs",
     list: [
       {
         name: "Lorna Macdonald",
@@ -86,11 +86,286 @@ const committee = [
       {
         name: "Zixin Wang",
         institution: "The University of Queensland, Australia",
-        imageUrl: "",
+        imageUrl: "Zixin Wang_cropped",
       },
       {
         name: "Yiyun Zhang",
         institution: "The University of Queensland, Australia",
+        imageUrl: "Yiyun",
+      },
+    ],
+  },
+  {
+    role: "Publicity Chairs",
+    list: [
+      {
+        name: "Yang Yang",
+        institution: "",
+        imageUrl: "Yang Yang_cropped",
+      },
+      {
+        name: "Hanwang Zhang",
+        institution: "Nanyang Technological University, Singapore",
+        imageUrl: "Hanwang Zhang_cropped",
+      },
+      {
+        name: "Wen-Huang Cheng",
+        institution: "",
+        imageUrl: "Wen-Huang Cheng",
+      },
+    ],
+  },
+  {
+    role: "Panel Chairs",
+    list: [
+      {
+        name: "Jiebo Luo",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Liqiang Nie",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Award Chairs",
+    list: [
+      {
+        name: "Chua Tat Seng",
+        institution: "National University of Singapore, Singapore",
+        imageUrl: "",
+      },
+      {
+        name: "Susanne Boll",
+        institution: "University of Oldenburg, Germany",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Short Paper & Demo Chairs",
+    list: [
+      {
+        name: "Lei Zhu",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Zhineng Chen",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Min Xu",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Workshop Chairs",
+    list: [
+      {
+        name: "Yanli Ji",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Zheng Wang",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Tutorial Chairs",
+    list: [
+      {
+        name: "Liang Zheng",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Xin Yu",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Brave New Idea Chairs",
+    list: [
+      {
+        name: "Chee Seng Chan",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Ichiro Ide",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Quanzeng You",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Grand Challenge Chairs",
+    list: [
+      {
+        name: "Shin'ichi Satoh",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Jingkuan Song",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Applied Research Track Chairs",
+    list: [
+      {
+        name: "Yong Man Ro",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Gan Chuang",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Diversity Chairs",
+    list: [
+      {
+        name: "Jia Jia",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Marie Boden",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Si Liu",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "PhD School Coordinators",
+    list: [
+      {
+        name: "Mahsa Baktashmotlagh",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Zheng Zhang",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Tianran Hu",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Student Travel Grant Chairs",
+    list: [
+      {
+        name: "Gunhee Kim",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Xinshun Xu",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Finance & Registration Chair",
+    list: [
+      {
+        name: "Kathleen Williamson",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Sponsorships Chairs",
+    list: [
+      {
+        name: "Xiaofang Zhou",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Bing-Kun Bao",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Volunteer Coordination Chairs",
+    list: [
+      {
+        name: "Yadan Luo",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Ziwei Wang",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Proceedings Chairs",
+    list: [
+      {
+        name: "Jingjing Li",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Xinchen Liu",
+        institution: "",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    role: "Special Session Chairs",
+    list: [
+      {
+        name: "Jiaying Liu",
+        institution: "",
+        imageUrl: "",
+      },
+      {
+        name: "Yi Yang",
+        institution: "",
         imageUrl: "",
       },
     ],
