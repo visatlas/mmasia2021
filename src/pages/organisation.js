d3c7e7f313b8a0caa9f993eddf4fc10504d00a08
Nifu <frzmit@gmail.com>
2021-06-22 22:50:30 +1000

    update footer sponsorship

--- src/pages/organisation.js
+++ src/pages/organisation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
