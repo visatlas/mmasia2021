72a4980b0283fd289c8010a976a9a4b8461b4d92
Zi-Xin Wong <46414159+Jo-wang@users.noreply.github.com>
2021-06-28 21:36:55 +1000

    Update email address

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -64,8 +64,8 @@ const Sponsors = ({ data, location }) => {
 
               <p className="mt-4 mb-6">ACM Multimedia Asia 2021 Sponsorships Chairs:</p>
               <ul className="list-disc list-inside">
-                <li className="mb-2"><strong>Xiaofang Zhou</strong>, Hong Kong University of Science and Technology, China</li>
-                <li className="mb-6"><strong>Bing-Kun Bao</strong>, Nanjing University of Posts and Telecommunications, China</li>
+                <li className="mb-2"><strong>Xiaofang Zhou</strong> (<address><a href="zxf@itee.uq.edu.au">zxf@itee.uq.edu.au</a></address>), Hong Kong University of Science and Technology, China</li>
+                <li className="mb-6"><strong>Bing-Kun Bao</strong> (<address><a href="bingkunbao@njupt.edu.cn">bingkunbao@njupt.edu.cn</a></address>), Nanjing University of Posts and Telecommunications, China</li>
               </ul>
 
               <footer className="pb-0">
