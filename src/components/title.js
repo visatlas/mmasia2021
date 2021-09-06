840d29b06cd47966e5ae15c3b56cf8d4057a13f8
Nifu <frzmit@gmail.com>
2021-09-07 00:46:39 +1000

    Add hybrid

--- src/components/title.js
+++ src/components/title.js
@@ -49,8 +49,10 @@ export default function Title({ allowVideo }) {
           position: "absolute", top: "0%", right: "20%",
           backgroundColor: "rgba(236, 231, 241, 0.85)"
         }}>
-          <h1 className="mb-2 font-titleFont text-black font-extrabold tracking-wide text-titleBig leading-titleBig">
-            <span className="text-mainPurple">ACM</span> <br />Multimedia <br />Asia 2021<sup className="mb-4 font-regular text-titleSmall">Hybrid</sup></h1>
+          <h1 className="mb-2 relative font-titleFont text-black font-extrabold tracking-wide text-titleBig leading-titleBig">
+            <span className="text-mainPurple">ACM</span>&nbsp;<br />Multimedia&nbsp;<br />Asia 2021
+            <span className="absolute bottom-0 right-0 border border-mainPurple text-mainPurple font-semibold mr-6 mb-2 px-1 py-0.5 rounded-sm text-xs tracking-tight font-sans">Hybrid</span>
+          </h1>
           <p className="mb-4 font-semibold text-titleSmall">Gold Coast, Australia</p>
           <p className="mb-1 font-bold text-mainPurple text-titleMedium tracking-semiWide">1 - 3 December, 2021</p>
           <div className="flex space-x-2 w-full mb-7 h-7">
@@ -94,8 +96,10 @@ export default function Title({ allowVideo }) {
       <div className="md:hidden">
         <div className="global-wrapper pt-9 bg-purple-50">
           <div className="flex flex-col items-center">
-            <h1 className="mb-2 font-titleFont text-black font-extrabold tracking-wide text-titleBig leading-titleBig text-center">
-              <span className="text-mainPurple">ACM</span> <br />Multimedia <br />Asia 2021</h1>
+            <h1 className="mb-2 w-87 relative font-titleFont text-black font-extrabold tracking-wide text-titleBig leading-titleBig text-center">
+              <span className="text-mainPurple">ACM</span>&nbsp;<br />Multimedia&nbsp;<br />Asia 2021
+              <span className="absolute bottom-0 right-0 border border-mainPurple text-mainPurple font-semibold mr-3 mb-2 px-1 py-0.5 rounded-sm text-xs tracking-tight font-sans">Hybrid</span>
+            </h1>
             <p className="mb-4 font-semibold text-titleSmall" >Gold Coast, Australia</p>
             <p className="mb-1 font-bold text-mainPurple text-titleMedium tracking-semiWide">1 - 3 December, 2021</p>
             <div className="flex space-x-2 mb-7 h-7">
