442f7a76a7411d4bfba75ca063e2c6fab3c567c9
Nifu <frzmit@gmail.com>
2021-11-04 15:20:18 +1000

    Implement time zone

--- src/components/program/dashboard.js
+++ src/components/program/dashboard.js
@@ -1,10 +1,19 @@
 import React, { Fragment, useEffect, useState } from "react";
+import TimezoneSelect from "react-timezone-select";
 import { Link } from "gatsby";
 import { getUser } from "../../services/auth";
 
 const Dashboard = () => {
   // Get the sessions data
   const [sessions, setSessions] = useState([]);
+  const [timezone, setTimezone] = useState(
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  );
+
+  const convertTZ = (date, tzString) => {
+    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", { timeZone: tzString }));
+  };
+
   useEffect(() => {
     fetch(`https://mmasia2021.uqcloud.net/api/sessions`, {
       method: "GET",
@@ -16,34 +25,51 @@ const Dashboard = () => {
       .catch(err => console.log(err));
   }, []);
 
-  const timeOffset = 10;
-  const scale = 2;
+  // const timeOffset = 10;
+  // const scale = 2;
 
-  const Session = ({ id, start, end, day, name }) => {
-    const startRow = (start - (timeOffset - 1)) * scale + 1;
-    const endRow = startRow + (end - start) * scale;
+  // const Session = ({ id, start, end, day, name }) => {
+  //   const startRow = (start - (timeOffset - 1)) * scale + 1;
+  //   const endRow = startRow + (end - start) * scale;
 
-    return (
-      <Link to={`/program/session/${id}`}
-        className="bg-gray-600 hover:bg-gray-500 duration-100 rounded-md font-semibold text-sm text-gray-100 p-1"
-        style={{
-          "gridRowStart": startRow,
-          "gridRowEnd": endRow,
-          "gridColumnStart": day + 1
-        }}>
-        <span className="">
-          {name}
-        </span>
-      </Link>
-    );
-  };
+  //   return (
+  //     <Link to={`/program/session/${id}`}
+  //       className="bg-gray-600 hover:bg-gray-500 duration-100 rounded-md font-semibold text-sm text-gray-100 p-1"
+  //       style={{
+  //         "gridRowStart": startRow,
+  //         "gridRowEnd": endRow,
+  //         "gridColumnStart": day + 1
+  //       }}>
+  //       <span className="">
+  //         {name}
+  //       </span>
+  //     </Link>
+  //   );
+  // };
 
   return (
     <div className="global-wrapper py-10">
       <h1 className="text-4xl mb-10 font-extrabold font-headingStyle tracking-semiWide text-semiBlack">Program</h1>
       <h2 className="text-xl mb-6 font-bold font-headingStyle">Announcement</h2>
       <h2 className="text-xl mb-6 font-bold font-headingStyle">Event Schedule</h2>
-      <div className="bg-gray-100 p-4 rounded-lg">
+
+      <div className="mb-6">
+        <TimezoneSelect value={timezone} onChange={setTimezone} />
+      </div>
+
+      {sessions.map((session, index) => {
+        return (
+          <Fragment key={index}>
+            <Link to={`/program/session/${session.id}`}>
+              <p className="mb-0">{convertTZ(session.start, timezone.value).toLocaleString([],
+                { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' }
+              )} - {session.name}</p>
+            </Link>
+          </Fragment>
+        );
+      })}
+
+      {/* <div className="bg-gray-100 p-4 rounded-lg">
         <div className="grid gap-8 grid-rows-1 mb-2 place-items-center font-semibold"
           style={{ "gridTemplateColumns": "50px auto auto auto" }}>
           <div className="col-start-2">Wed 1 Dec</div>
@@ -51,7 +77,7 @@ const Dashboard = () => {
           <div className="">Fri 3 Dec</div>
         </div>
 
-        <div className="h-96 grid grid-flow-col gap-x-8 gap-y-1"
+        <div className="relative h-96 grid grid-flow-col gap-x-8 gap-y-1"
           style={{
             "gridTemplateColumns": "50px auto auto auto",
             "gridTemplateRows": `repeat(${scale * 9}, minmax(0, 1fr))`
@@ -60,13 +86,17 @@ const Dashboard = () => {
             <div key={index} className="row-span-2 flex justify-end font-semibold">
               {num + timeOffset - 1}:00
             </div>))}
-          {sessions.map((session, index) => (
+
+          {sessions.length ? sessions.map((session, index) => (
             <Fragment key={index}>
               <Session id={session.id} start={session.start} end={session.end} day={session.day} name={session.name} />
             </Fragment>
-          ))}
+          )) : (
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+            z-50 font-semibold text-xl text-gray-400">Loading...</div>
+          )}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
